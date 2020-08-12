c17a69d37bffb55e56de7c5a3a6e7cd340c037b2
Deric Huo <dehuo@usc.edu>
2020-08-11 22:52:43 -0700

    Update and rename test.py to spectral_clustering.py

--- test/spectral_clustering.py
+++ test/spectral_clustering.py
@@ -1,8 +1,4 @@
 #coding:utf-8
-"""
-谱聚类算法
-核心思想：构建样本点的图，切分图，使得子图内权重最大，子图间权重最小
-"""
 import numpy as np
 from sklearn.cluster import KMeans
 
@@ -12,22 +8,21 @@ class Spectrum:
         self.n_cluster = n_cluster
         self.tol = tol
         self.max_iter = max_iter
-        self.method = method  # 本程序提供规范化以及非规范化的谱聚类算法
-        self.criterion = criterion  # 相似性矩阵的构建方法
-        self.gamma = gamma  # 高斯方法中的sigma参数
-        self.dis_epsilon = dis_epsilon  # epsilon-近邻方法的参数
-        self.k = k  # k近邻方法的参数
+        self.method = method  
+        self.criterion = criterion  
+        self.gamma = gamma  
+        self.dis_epsilon = dis_epsilon  
+        self.k = k  
 
-        self.W = None  # 图的相似性矩阵
-        self.L = None  # 图的拉普拉斯矩阵
-        self.L_norm = None  # 规范化后的拉普拉斯矩阵
-        self.D = None  # 图的度矩阵
+        self.W = None  
+        self.L = None  
+        self.L_norm = None  
+        self.D = None  
         self.cluster = None
 
         self.N = None
 
     def init_param(self, data):
-        # 初始化参数
         self.N = data.shape[0]
         dis_mat = self.cal_dis_mat(data)
         self.cal_weight_mat(dis_mat)
@@ -36,7 +31,6 @@ class Spectrum:
         return
 
     def cal_dis_mat(self, data):
-        # 计算距离平方的矩阵
         dis_mat = np.zeros((self.N, self.N))
         for i in range(self.N):
             for j in range(i + 1, self.N):
@@ -46,11 +40,11 @@ class Spectrum:
 
     def cal_weight_mat(self, dis_mat):
         # 计算相似性矩阵
-        if self.criterion == 'gaussian':  # 适合于较小样本集
+        if self.criterion == 'gaussian': 
             if self.gamma is None:
                 raise ValueError('gamma is not set')
             self.W = np.exp(-self.gamma * dis_mat)
-        elif self.criterion == 'k_nearest':  # 适合于较大样本集
+        elif self.criterion == 'k_nearest':  
             if self.k==0 :
                 self.W = np.exp(-self.gamma * dis_mat)
             else:
@@ -58,14 +52,14 @@ class Spectrum:
                     raise ValueError('k or gamma is not set')
                 self.W = np.zeros((self.N, self.N))
                 for i in range(self.N):
-                    inds = np.argpartition(dis_mat[i], self.k + 1)[:self.k + 1]  # 由于包括自身，所以+1
+                    inds = np.argpartition(dis_mat[i], self.k + 1)[:self.k + 1]  
                     tmp_w = np.exp(-self.gamma * dis_mat[i][inds])
                     self.W[i][inds] = tmp_w
                 for i in range(self.N):
                     for j in range(i + 1, self.N):
                         if  self.W[i,j]==0 and self.W[j,i]!=0 :
                             self.W[i,j]=self.W[j,i]=0
-        elif self.criterion == 'eps_nearest':  # 适合于较大样本集
+        elif self.criterion == 'eps_nearest':  
             if self.dis_epsilon is None:
                 raise ValueError('epsilon is not set')
             self.W = np.zeros((self.N, self.N))
@@ -77,7 +71,6 @@ class Spectrum:
         return
 
     def fit(self, data):
-        # 训练主函数
         self.init_param(data)
         if self.method == 'unnormalized':
             w, v = np.linalg.eig(self.L)
